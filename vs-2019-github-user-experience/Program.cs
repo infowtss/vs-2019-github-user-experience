ba9758cea85fb81988821ab813548ba5f43955d7
infowtss <infowtss@gmail.com>
2023-10-27 13:55:59 +0530

    Add project files.

---
+++ vs-2019-github-user-experience/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vs_2019_github_user_experience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
